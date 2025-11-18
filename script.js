c25ba6b74d3d3b85413fdac757653a9acae1bd52
o1abdurakhimow <ibocheek777@gmail.com>
2025-11-18 14:51:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let name = prompt("Ismingiz nima?");
+let id = prompt("Space ID'ingiz nima?");
+let password = prompt("Space parolingiz nima?");
+
+console.log({
+    name: name,
+    id: id,
+    password: password
+});
